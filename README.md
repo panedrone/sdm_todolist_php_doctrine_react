caccb799eeec7b71115fea3428c303cf5d750815
panedrone <sqldalmaker@gmail.com>
2022-07-05 17:18:59 +0300

    reviews

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # sdm_demo_todolist_php_doctrine
-Quick Demo of how to use [SQL DAL Maker](https://github.com/panedrone/sqldalmaker) + PHP + Doctrine models + DTO and DAO classes for Doctrine raw-SQL.
+Quick Demo of how to use [SQL DAL Maker](https://github.com/panedrone/sqldalmaker) + PHP + Doctrine models + DAO classes which use Doctrine's raw-SQL under the hood.
 
 ![demo-go.png](demo-go.png)
 
--- app/cli.php
+++ app/cli.php
@@ -4,54 +4,59 @@ use dal\models\Group;
 
 require_once(__DIR__ . '/bootstrap.php');
 
-logger(); // just create it before usage
+try {
+    logger(); // just create it before usage
 
-$gr = new Group();
-$gr->set_g_name("Hello from Doctrine " . date("Y-m-d H:i:s"));
-em()->persist($gr);
-em()->flush();
+    $gr = new Group();
+    $gr->set_g_name("Hello from Doctrine " . date("Y-m-d H:i:s"));
+    em()->persist($gr);
+    em()->flush();
 
-$g_id = $gr->get_g_id();  // generated id is available!
-print_r($g_id . PHP_EOL);
+    $g_id = $gr->get_g_id();  // generated id is available!
+    print_r($g_id . PHP_EOL);
 
-$dao = groups_dao(); // raw-SQL
-$groups = $dao->get_groups(); // code-completion is OK
-print "Groups: " . print_r($groups, true) . PHP_EOL;
+    $dao = groups_dao(); // raw-SQL
+    $groups = $dao->get_groups(); // code-completion is OK
+    print "Groups: " . print_r($groups, true) . PHP_EOL;
 
-$gr = find_group($g_id);
-print "get_g_name: " . $gr->get_g_name() . PHP_EOL; // code-completion is OK
-print "Group: " . print_r($gr, true) . PHP_EOL;
+    $gr = find_group($g_id);
+    print "get_g_name: " . $gr->get_g_name() . PHP_EOL; // code-completion is OK
+    print "Group: " . print_r($gr, true) . PHP_EOL;
 
-$gr = em()->getPartialReference(Group::class, $g_id);
-em()->remove($gr);
-em()->flush();
+    $gr = em()->getPartialReference(Group::class, $g_id);
+    em()->remove($gr);
+    em()->flush();
 
-$gr_tasks = get_group_tasks(21);
-print "get_t_subject: " . $gr_tasks[0]->get_t_subject() . PHP_EOL; // code-completion is OK
-print "group_tasks: " . print_r($gr_tasks, true) . PHP_EOL;
+    $gr_tasks = get_group_tasks(21);
+    print "get_t_subject: " . $gr_tasks[0]->get_t_subject() . PHP_EOL; // code-completion is OK
+    print "group_tasks: " . print_r($gr_tasks, true) . PHP_EOL;
 
 // ....................
 
-$rows_affected = $dao->rename_group("Hello from Doctrine " . date("Y-m-d H:i:s"), 66);
-print "rows_affected: " . print_r($rows_affected, true) . PHP_EOL;
+    $rows_affected = $dao->rename_group("Hello from Doctrine " . date("Y-m-d H:i:s"), 66);
+    print "rows_affected: " . print_r($rows_affected, true) . PHP_EOL;
 
-$gr = $dao->get_group(66);
-print "Group: " . print_r($gr, true) . PHP_EOL;
+    $gr = $dao->get_group(66);
+    print "Group: " . print_r($gr, true) . PHP_EOL;
 
-$group_ids = $dao->get_groups_ids(); // raw-SQL
-print "group_ids: " . print_r($group_ids, true) . PHP_EOL;
+    $group_ids = $dao->get_groups_ids(); // raw-SQL
+    print "group_ids: " . print_r($group_ids, true) . PHP_EOL;
 
-$g_id = $dao->get_group_id(21);
-print "g_id: " . print_r($g_id, true) . PHP_EOL;
+    $g_id = $dao->get_group_id(21);
+    print "g_id: " . print_r($g_id, true) . PHP_EOL;
 
 // ..... no code-completion :( ...............
 
-$groups = groups()->findAll();
-/** @var $gr Group */
-foreach ($groups as $gr) {
-    echo sprintf("-%s\n", $gr->get_g_name());
-}
+    $groups = groups()->findAll();
+    /** @var $gr Group */
+    foreach ($groups as $gr) {
+        echo sprintf("-%s\n", $gr->get_g_name());
+    }
 // https://blog.programster.org/getting-started-with-doctrine-orm
-print "Groups: " . print_r($groups, true) . PHP_EOL;
+    print "Groups: " . print_r($groups, true) . PHP_EOL;
 
-print_r(phpversion());
+    print_r(phpversion());
+
+} catch (\Exception $e) {
+    print_r($e);
+}
