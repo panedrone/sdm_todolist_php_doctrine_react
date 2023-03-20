998e04e63543baa9cc4ba46b1fc0cf43dc8747da
Andrii M <andrey.meretsky@jetsoftpro.com>
2023-03-20 19:47:51 +0200

    + validateDate

--- app/api/validators.php
+++ app/api/validators.php
@@ -18,6 +18,9 @@ function validate_task($data, $existing) : ?array
         if (!is_int($data->t_priority) || $data->t_priority < 1 || $data->t_priority > 10) {
             array_push($err, array("priority" => "need an integer 1..10"));
         }
+        if (!validateDate($data->t_date)) {
+            array_push($err, array("date" => "invalid date; expected format is 2020-12-31"));
+        }
     }
     if (count($err) == 0) {
         return null;
@@ -25,3 +28,9 @@ function validate_task($data, $existing) : ?array
     return $err;
 }
 
+function validateDate($date, $format = 'Y-m-d')
+{
+    $d = DateTime::createFromFormat($format, $date);
+    // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
+    return $d && $d->format($format) === $date;
+}
