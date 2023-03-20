39d21d089c5e4ccd99ef3c7d02760533dadf1792
Andrii M <andrey.meretsky@jetsoftpro.com>
2023-03-20 20:30:12 +0200

    + validateDate

--- app/api/validators.php
+++ app/api/validators.php
@@ -12,14 +12,14 @@ function validate_task($data, $existing) : ?array
 {
     $err = array();
     if ($data->t_subject == null || strlen(trim($data->t_subject)) == 0) {
-        array_push($err, array("subject" => "not set"));
+        array_push($err, array("Subject" => "not set"));
     }
     if ($existing) {
         if (!is_int($data->t_priority) || $data->t_priority < 1 || $data->t_priority > 10) {
-            array_push($err, array("priority" => "need an integer 1..10"));
+            array_push($err, array("Priority" => "need an integer 1..10"));
         }
         if (!validateDate($data->t_date)) {
-            array_push($err, array("date" => "invalid date; expected format is 2020-12-31"));
+            array_push($err, array("Date" => "invalid date; expected format is 2020-12-31"));
         }
     }
     if (count($err) == 0) {
